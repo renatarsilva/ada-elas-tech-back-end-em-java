26fc1f18f699843c59e8691103e1677161b16e0a
Renata <renata.rodriguesdasilva@outlook.com>
2025-09-27 19:51:09 -0300

    feat update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello World");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        byte variavelByte = 12;
+        short variavelShort = 300;
+        char variavelChar = 60000;
+        int variavelInt = 600000;
+        long variavelLong = 60000000L;
+
+        System.out.println(variavelByte);
+        System.out.println(variavelLong);
     }
 }
