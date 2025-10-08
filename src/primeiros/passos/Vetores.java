27b71f38c34ad7e998762b6c3cd27900e47c98eb
Renata <renata.rodriguesdasilva@outlook.com>
2025-10-08 16:03:30 -0300

    feat update vetores

---
+++ src/primeiros/passos/Vetores.java
@@ -0,0 +1,15 @@
+package primeiros.passos;
+
+public class Vetores {
+    public static void main(String[] args) {
+    int[] numeros = new int[3];
+
+    numeros [0] = 10;
+    numeros [1] = 20;
+    numeros [2] = 30;
+
+    for (int i = 0; i< numeros.length; i++){
+        System.out.println(numeros[i]);
+    }
+    }
+}
