5cf85369e5d33651f9cd0c3a2d59d1537c185197
Renata <renata.rodriguesdasilva@outlook.com>
2025-10-30 23:54:30 -0300

    feat aula04

---
+++ src/modulo1/aula04/ExemploCalculosTempo.java
@@ -0,0 +1,27 @@
+package modulo1.aula04;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class ExemploCalculosTempo {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime future = now.plusDays(10);
+
+        Duration duration = Duration.between(now, future);
+        System.out.println(duration.toDays());
+
+        LocalDate today = LocalDate.now();
+        LocalDate lastDay = LocalDate.of(2025, 12, 31);
+        Period period1 = Period.between(today, lastDay);
+
+        System.out.println(period1.getMonths());
+
+        System.out.println(ChronoUnit.DAYS.between(today, lastDay));
+
+
+    }
+}
---
+++ src/modulo1/aula04/ExemploZoneDateTime.java
@@ -0,0 +1,22 @@
+package modulo1.aula04;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZonedDateTime;
+
+public class ExemploZoneDateTime {
+    public static void main(String[] args) {
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+
+        Duration duration = Duration.ofHours(5);
+        System.out.println(duration);
+        System.out.println(zonedDateTime.plus(duration));
+
+        LocalDate dataNascimento = LocalDate.of(1995,8,23);
+        Period period = Period.between(dataNascimento, LocalDate.now());
+        System.out.println(period.getYears());
+    }
+}
