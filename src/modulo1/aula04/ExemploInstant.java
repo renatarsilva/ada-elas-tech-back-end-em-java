d774a104bd4a0bf99b1bdc5d808dfc532013ea49
Renata <renata.rodriguesdasilva@outlook.com>
2025-10-31 00:18:08 -0300

    feat aula04

---
+++ src/modulo1/aula04/ExemploInstant.java
@@ -0,0 +1,16 @@
+package modulo1.aula04;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class ExemploInstant {
+    public static void main(String[] args) throws InterruptedException {
+        Instant inicio = Instant.now();
+
+        Thread.sleep(2000);
+        Instant fim = Instant.now();
+        Duration duracao = Duration.between(inicio, fim);
+
+        System.out.println(duracao.toMillis());
+    }
+}
